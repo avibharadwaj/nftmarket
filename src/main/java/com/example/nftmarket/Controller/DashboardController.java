04f8c4ddfd45f1936508452ad102f2e9cc75bbd7
Sujith Kumaar <77604005+sk7878@users.noreply.github.com>
2022-12-15 17:29:20 -0800

    Added dashboard functionality

---
+++ src/main/java/com/example/nftmarket/Controller/DashboardController.java
@@ -0,0 +1,19 @@
+package com.example.nftmarket.Controller;
+
+import com.example.nftmarket.Service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping(value = "/dashboard")
+    public ResponseEntity<?> getDashboard() {
+        return dashboardService.getDashboard();
+    }
+}
--- src/main/java/com/example/nftmarket/Controller/PersonalTransactionsController.java
+++ src/main/java/com/example/nftmarket/Controller/PersonalTransactionsController.java
@@ -3,7 +3,6 @@ package com.example.nftmarket.Controller;
 import com.example.nftmarket.Entity.Users;
 import com.example.nftmarket.Repository.UsersRepo;
 import com.example.nftmarket.Service.PersonalTransactionsService;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +19,9 @@ public class PersonalTransactionsController {
     private UsersRepo userRepo;
 
     @GetMapping(value = "/myTransactions")
-    public ResponseEntity<?> getMyTransactions(Principal principal) throws JSONException {
-        //Users user = userRepo.findByEmail(principal.getName());
-        Users user = userRepo.findById(1).get();
+    public ResponseEntity<?> getMyTransactions(Principal principal) {
+        Users user = userRepo.findByEmail(principal.getName());
+        //Users user = userRepo.findById(1).get();
         return personalTransactionsService.getMyTransactions(user);
     }
 }
